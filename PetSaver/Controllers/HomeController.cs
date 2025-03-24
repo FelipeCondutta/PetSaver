93eda93b357855111ec8e47028d7b77ab9125cc0
JesusDoAlmoço <JesusDoAlmoo@DESKTOP-F2EPAOV>
2025-03-24 11:50:50 -0300

    Adicionar arquivos de projeto.

---
+++ PetSaver/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PetSaver.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(); // Retorna a View "Index.cshtml" dentro de Views/Home/
+        }
+    }
+
+
+
+}
